ef527e6203bcd087468fc9600a7b68e46d4344fd
armie <armiejanedelosrys@gmail.com>
2024-04-11 22:39:25 +0800

    new changes sa education

---
+++ port_script.js
@@ -0,0 +1,9 @@
+// function bounce() {
+//     var marquee = document.getElementById("myMarquee");
+//     marquee.setAttribute("behavior", "alternate");
+// }
+
+// function stopBounce() {
+//     var marquee = document.getElementById("myMarquee");
+//     marquee.setAttribute("behavior", "scroll");
+// }
